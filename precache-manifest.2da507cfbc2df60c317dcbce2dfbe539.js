c5ca1c2c43b59ce50bed0c571403fd8256d0dcd6
Kei95 <kei0613shine.star@gmail.com>
2019-09-11 11:16:01 -0700

    Updates

---
+++ precache-manifest.2da507cfbc2df60c317dcbce2dfbe539.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8053f95863dff5f012a935d7e102cb05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a20442ad270c53bab140",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "d5f623558a570b59e5e4",
+    "url": "/static/css/main.fc9b5d06.chunk.css"
+  },
+  {
+    "revision": "a20442ad270c53bab140",
+    "url": "/static/js/2.d83942a7.chunk.js"
+  },
+  {
+    "revision": "d5f623558a570b59e5e4",
+    "url": "/static/js/main.64724d29.chunk.js"
+  },
+  {
+    "revision": "fe31ec383894d9f98d66",
+    "url": "/static/js/runtime~main.a968dc45.js"
+  },
+  {
+    "revision": "b52b807a2fef76e2b7089d9922d4ef03",
+    "url": "/static/media/Thunderstorm.b52b807a.jpg"
+  },
+  {
+    "revision": "6607a363714df42742c9764b5c49519b",
+    "url": "/static/media/cloud.6607a363.jpg"
+  },
+  {
+    "revision": "481761580438dd1abe4db2e56bace0b3",
+    "url": "/static/media/fog.48176158.jpg"
+  },
+  {
+    "revision": "b6a831fdb4db51bf46094ffdc3ac04e4",
+    "url": "/static/media/rain.b6a831fd.jpg"
+  },
+  {
+    "revision": "97e70aac0aa4e9a0b5bfd054ad7db415",
+    "url": "/static/media/snow.97e70aac.jpg"
+  },
+  {
+    "revision": "34ebe320c0dcab77ef056b80f8c19f4e",
+    "url": "/static/media/sunny.34ebe320.jpg"
+  }
+]);
